b69c564a13d11739ddb37ca3718ed5d690bb1746
906956368@qq.com <lcs1989!@#>
2019-08-08 18:46:32 +0800

    增加docker日志显示

--- src/view/server-list/server-list.vue
+++ src/view/server-list/server-list.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+<!--    服务器显示列表-->
     <div class="grid">
       <Card :bordered="false" v-for="(item,k) in server_list" :key="k" class="grid-card">
         <p slot="title" class="grid-card-title">{{item.ip}}</p>
@@ -16,8 +17,8 @@
           <i-Col span="12">
             <p v-if="!item.isDocker">NO</p>
             <ButtonGroup  v-if="item.isDocker" class="grid-card-row-col-button">
-              <Button type="info" size="small" @click="handleDockerTagClick(item.ip)">Images</Button>
-              <Button type="success" size="small" @click="handleDockerTagClick(item.ip)">Containers</Button>
+              <Button type="info" size="small" @click="handleDockerImageClick(item.ip)">Images</Button>
+              <Button type="success" size="small" @click="handleDockerContainerClick(item.ip)">Containers</Button>
             </ButtonGroup>
           </i-Col>
         </Row>
@@ -31,7 +32,7 @@
         </Row>
       </Card>
     </div>
-
+<!--    抽屉显示容器信息-->
     <Drawer :title="drawer.title" width="640" :draggable="true"  v-model="drawer.flag" class="drawer">
       <div class="drawer-header">
         <Row class="drawer-header-row">
@@ -40,7 +41,7 @@
         </Row>
         <Row class="drawer-header-row">
           <i-Col span="8" class="drawer-header-row-name">Running</i-Col>
-          <i-Col span="8" class="drawer-header-row-name">Stopped</i-Col>
+          <i-Col span="8" class="drawer-header-row-name">Exited</i-Col>
           <i-Col span="8" class="drawer-header-row-name">Paused</i-Col>
         </Row>
         <Row class="drawer-header-row">
@@ -54,10 +55,13 @@
         <Collapse accordion>
           <Panel v-for="(item,k) in dockerInfo.containerList" :key="k" :name="item.id">
             {{item.names[0].substring(1)}}
+            <Tag v-if="item.state==='running'" color="blue">Running</Tag>
+            <Tag v-if="item.state==='exited'" color="red">Exited</Tag>
+            <Tag v-if="item.state==='paused'" color="orange">Paused</Tag>
             <Card slot="content" class="drawer-details-card">
               <Row class="drawer-details-card-row">
                 <i-Col span="4">id</i-Col>
-                <i-Col span="18">{{item.id}}</i-Col>
+                <i-Col span="18">{{item.id.substring(0,12)}}</i-Col>
               </Row>
               <Row class="drawer-details-card-row">
                 <i-Col span="4">image</i-Col>
@@ -91,7 +95,7 @@
                     <Button size="small" type="info" @click="handleContainerStop(item.id, item.state)">stop</Button>
                     <Button size="small" type="success" @click="handleContainerRestart(item.id)">restart</Button>
                     <Button size="small" type="error" @click="handleContainerRemove(item.id)">remove</Button>
-                    <Button size="small" type="dashed">show log</Button>
+                    <Button size="small" type="dashed" @click="handleShowLog(item.id)">show log</Button>
                   </ButtonGroup>
                 </i-Col>
               </Row>
@@ -100,18 +104,57 @@
         </Collapse>
       </div>
     </Drawer>
-
+<!--    删除容器提示框-->
     <Modal
-      v-model="removeModal.flag"
+      v-model="removeModel.flag"
       title="please confirm"
-      @on-ok="removeContainerOK(removeModal.containerId)">
+      @on-ok="removeContainerOK(removeModel.containerId)">
       <p>Are you sure doing this？this cannot redo！</p>
+    </Modal>
+<!--    显示日志对话框-->
+    <Modal
+      v-model="logModel.flag"
+      title="选择开始日期和行数"
+      @on-ok="handleLogModel">
+      <Row class="logModel-row">
+        <i-Col span="6">
+          日期
+        </i-Col>
+        <i-Col span="18">
+          <DatePicker type="datetime" format="yyyy-MM-dd HH:mm:ss" value="yyyy-MM-dd HH:mm:ss" size="small" placeholder="Select date" style="width: 200px" @on-change="setDateTime"></DatePicker>
+        </i-Col>
+      </Row>
+      <Row>
+        <i-Col span="6">
+          显示行数
+        </i-Col>
+        <i-Col span="18">
+          <Input type="number" v-model="logModel.tail" size="small" placeholder="Enter something..." style="width: 200px"/>
+        </i-Col>
+      </Row>
+    </Modal>
+
+<!--    日志显示-->
+    <Modal
+      v-model="logDetail.flag"
+      title="日志信息"
+      fullscreen
+      footer-hide>
+      <pre class="logDetail-pre">{{logDetail.log}}</pre>
     </Modal>
   </div>
 </template>
 
 <script>
-import { getServerList, getDockerInfo, startContainer, stopContainer, restartContainer, removeContainer } from '@/api/data'
+import {
+  getServerList,
+  getDockerInfo,
+  startContainer,
+  stopContainer,
+  restartContainer,
+  removeContainer,
+  getContainerLog,
+  getImageList } from '@/api/data'
 export default {
   name: 'server_list_page',
   data () {
@@ -126,9 +169,21 @@ export default {
       // 某个服务器对应的docker信息
       dockerInfo: {},
       // 删除容器模态提示框
-      removeModal: {
+      removeModel: {
         flag: false,
         containerId: ''
+      },
+      // 显示日志对话模态框
+      logModel: {
+        flag: false,
+        id: '',
+        dateTime: '',
+        tail: 0
+      },
+      // 日志
+      logDetail: {
+        flag: false,
+        log: ''
       }
     }
   },
@@ -148,8 +203,19 @@ export default {
         })
       })
     },
-    // 显示服务器的docker信息
-    handleDockerTagClick (ip) {
+    // 显示服务器的docker的镜像信息
+    handleDockerImageClick (ip) {
+      getImageList(ip).then(res => {
+        console.log(res.data.data)
+      }).catch(data => {
+        this.$Notice.error({
+          title: 'error',
+          desc: data
+        })
+      })
+    },
+    // 显示服务器的docker的容器信息
+    handleDockerContainerClick (ip) {
       getDockerInfo(ip).then(res => {
         const { data } = res.data
         this.dockerInfo = data
@@ -177,6 +243,8 @@ export default {
               title: 'success',
               desc: 'start container success！'
             })
+            // 刷新drawer
+            this.handleDockerContainerClick(this.drawer.ip)
           } else {
             this.$Notice.error({
               title: 'error',
@@ -205,6 +273,8 @@ export default {
               title: 'success',
               desc: 'container has stopped！'
             })
+            // 刷新drawer
+            this.handleDockerContainerClick(this.drawer.ip)
           } else {
             this.$Notice.error({
               title: 'error',
@@ -227,6 +297,8 @@ export default {
             title: 'success',
             desc: 'container restart success！'
           })
+          // 刷新drawer
+          this.handleDockerContainerClick(this.drawer.ip)
         } else {
           this.$Notice.error({
             title: 'error',
@@ -242,8 +314,8 @@ export default {
     },
     // 删除某个容器
     handleContainerRemove (id) {
-      this.removeModal.flag = true
-      this.removeModal.containerId = id
+      this.removeModel.flag = true
+      this.removeModel.containerId = id
     },
     // 删除某个容器模态框点击确定
     removeContainerOK (id) {
@@ -253,6 +325,8 @@ export default {
             title: 'success',
             desc: 'container restart success！'
           })
+          // 刷新drawer
+          this.handleDockerContainerClick(this.drawer.ip)
         } else {
           this.$Notice.error({
             title: 'error',
@@ -265,6 +339,39 @@ export default {
           desc: data
         })
       })
+    },
+    // 显示日志
+    handleShowLog (id) {
+      this.logModel.flag = true
+      this.logModel.id = id
+    },
+    // 日志选择日期时间时触发设置logModel.dateTime字段
+    setDateTime (date) {
+      this.logModel.dateTime = date
+    },
+    // 获取日志信息
+    handleLogModel () {
+      console.log(this.logModel.dateTime)
+      if (this.logModel.dateTime && this.logModel.tail) {
+        getContainerLog(this.drawer.ip, this.logModel.id, this.logModel.dateTime, this.logModel.tail).then(res => {
+          if (res.data.ok) {
+            this.logDetail.flag = true
+            this.logDetail.log = res.data.data
+          } else {
+            this.$Notice.error({
+              title: 'error',
+              desc: res.data.msg
+            })
+          }
+        }).catch(data => {
+          this.$Notice.error({
+            title: 'error',
+            desc: data
+          })
+        })
+      } else {
+        this.$Message.warning('please write all data!')
+      }
     }
   },
   mounted () {
@@ -320,5 +427,11 @@ export default {
       }
     }
   }
+}
+.logModel-row {
+  margin-bottom: 5px;
+}
+.logDetail-pre {
+
 }
 </style>
